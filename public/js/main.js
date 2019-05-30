f386513d55a065bbb33b59abc34219a1248640fb
citizenofearth <namgyal@gmail.com>
2019-05-30 19:16:52 -0400

    Add smooth scrolling internal anchors and update hexo

--- public/js/main.js
+++ public/js/main.js
@@ -26,6 +26,36 @@ $(document).ready(function(){
       $('#menu-icon, #menu-icon-tablet').removeClass('active');
       return false;
     }
+
+    // Vanilla JavaScript Scroll to Anchor
+    // @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+    (function() {
+      scrollTo();
+    })();
+
+    function scrollTo() {
+      const links = document.querySelectorAll('.headerlink');
+      links.forEach(each => (each.onclick = scrollAnchors));
+    }
+
+    function scrollAnchors(e, respond = null) {
+      const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+      e.preventDefault();
+      var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+      const targetAnchor = document.querySelector(targetID);
+      if (!targetAnchor) return;
+      const originalTop = distanceToTop(targetAnchor);
+      window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+      const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+          targetAnchor.tabIndex = '-1';
+          targetAnchor.focus();
+          window.history.pushState('', '', targetID);
+          clearInterval(checkIfDone);
+        }
+      }, 100);
+    }
   });
 
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
